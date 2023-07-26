20e552793d1d6f455de56394b9e1a53c8d9792df
Mubbashir Mustafa <mubbashir10@gmail.com>
2023-07-27 03:50:31 +0500

    disable search enging indexing

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,15 @@
 import "./globals.css";
+import type { Metadata } from "next";
 import { Roboto } from "next/font/google";
 
 const roboto = Roboto({
   weight: "400",
   subsets: ["latin"],
 });
+
+export const metadata: Metadata = {
+  robots: "noindex",
+};
 
 export default function RootLayout({
   children,
